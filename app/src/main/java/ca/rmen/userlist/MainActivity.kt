11fc2f1db6a7f429348d1f691d8f88e9d1c422ef
Carmen Alvarez <calvarez@libon.com>
2021-11-20 12:01:00 +0100

    Issue #3: Add databinding

--- app/src/main/java/ca/rmen/userlist/MainActivity.kt
+++ app/src/main/java/ca/rmen/userlist/MainActivity.kt
@@ -1,11 +1,13 @@
 package ca.rmen.userlist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ca.rmen.userlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
