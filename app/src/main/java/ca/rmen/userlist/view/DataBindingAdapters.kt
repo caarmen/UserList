f5bc0020fe9333fe8f1e45be1a0026bf4a41db02
Carmen <carmen@rmen.ca>
2021-11-20 13:49:27 +0100

    Issue #5: Display the user avatar with glide

---
+++ app/src/main/java/ca/rmen/userlist/view/DataBindingAdapters.kt
@@ -0,0 +1,13 @@
+package ca.rmen.userlist.view
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object DataBindingAdapters {
+    @BindingAdapter("app:imageUrl")
+    @JvmStatic
+    fun setImageUrl(view: ImageView, url: String) {
+        Glide.with(view.context).load(url).into(view)
+    }
+}
