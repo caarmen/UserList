54990679c0f25b0e1e086dd849c706af891e688b
Carmen <carmen@rmen.ca>
2021-11-20 17:25:19 +0100

    Issue #8: Add one test for the user details screen

--- app/src/main/java/ca/rmen/userlist/view/DataBindingAdapters.kt
+++ app/src/main/java/ca/rmen/userlist/view/DataBindingAdapters.kt
@@ -9,7 +9,7 @@ import com.bumptech.glide.Glide
 object DataBindingAdapters {
     @BindingAdapter("app:imageUrl")
     @JvmStatic
-    fun setImageUrl(view: ImageView, url: String) {
+    fun setImageUrl(view: ImageView, url: String?) {
         Glide.with(view.context).load(url).into(view)
     }
 
