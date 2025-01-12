f79a923108753cb6cda7d507754934caafb92ea2
Carmen Alvarez <carmen@rmen.ca>
2025-01-12 06:56:29 +0100

    Use AsyncTask to avoid blocking the main thread when fetching user data.

--- src/ca/rmen/userlist/UserListActivity.java
+++ src/ca/rmen/userlist/UserListActivity.java
@@ -5,20 +5,35 @@ import java.util.List;
 import ca.rmen.userlist.R;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 public class UserListActivity extends Activity {
 
+	private static final String TAG = UserListActivity.class.getSimpleName();
 	private UserRepository mRepository = new UserRepository();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		ListView listView = (ListView) findViewById(R.id.listView1);
-		List<UserModel> users = mRepository.fetchUsers();
-		listView.setAdapter(new UserListAdapter(UserListActivity.this, users));
+		final ListView listView = (ListView) findViewById(R.id.listView1);
+
+		new AsyncTask<Void, Void, List<UserModel>>() {
+
+			@Override
+			protected List<UserModel> doInBackground(Void... params) {
+				return mRepository.fetchUsers();
+			}
+
+			@Override
+			protected void onPostExecute(List<UserModel> users) {
+				Log.v(TAG, "Got user list");
+				listView.setAdapter(new UserListAdapter(UserListActivity.this, users));
+			}
+		}.execute();
 	}
 
 }
--- src/ca/rmen/userlist/UserListAdapter.java
+++ src/ca/rmen/userlist/UserListAdapter.java
@@ -5,6 +5,9 @@ import java.util.List;
 import ca.rmen.userlist.R;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class UserListAdapter extends ArrayAdapter<UserModel> {
+	private static final String TAG = UserListAdapter.class.getSimpleName();
 
 	private final List<UserModel> mUsers;
 
@@ -27,16 +31,46 @@ public class UserListAdapter extends ArrayAdapter<UserModel> {
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		UserModel user = mUsers.get(position);
-		View result = convertView;
-		if (result == null) {
-			result = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item, null);
-		}
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		final UserModel user = mUsers.get(position);
+		// Get or create our user item view.
+		final View result = convertView == null
+				? LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item, null)
+				: convertView;
+
+		// Set the name.
 		TextView nameView = (TextView) result.findViewById(R.id.name);
-		ImageView avatarView = (ImageView) result.findViewById(R.id.avatar);
+		final ImageView avatarView = (ImageView) result.findViewById(R.id.avatar);
 		nameView.setText(user.name.first + " " + user.name.last);
-		avatarView.setImageBitmap(UserRepository.getAvatar(user));
+
+		// Set the image. Fetch the image in the background.
+		AsyncTask<UserModel, Void, Bitmap> previousTask = (AsyncTask<UserModel, Void, Bitmap>) result.getTag();
+		if (previousTask != null)
+			previousTask.cancel(true);
+		AsyncTask<UserModel, Void, Bitmap> avatarTask = new AsyncTask<UserModel, Void, Bitmap>() {
+
+			@Override
+			protected Bitmap doInBackground(UserModel... user) {
+				Log.d(TAG, "doInBackground, position = " + position + ", user = " + user[0].name.first);
+				return UserRepository.getAvatar(user[0]);
+			}
+
+			@Override
+			protected void onPostExecute(Bitmap bitmap) {
+				Log.d(TAG, "onPostExecute, isShown = " + result.isShown() + ", position = " + position + ", user = "
+						+ user.name.first);
+				// If the user is scrolling quickly, this view may not be for
+				// this avatar anymore.
+				// Libraries like Picasso and Glide handle this nicely for us,
+				// but they require more recent versions of Android.
+				if (!isCancelled()) {
+					avatarView.setImageBitmap(bitmap);
+				}
+			}
+
+		};
+		result.setTag(avatarTask);
+		avatarTask.execute(user);
 		return result;
 	}
 
