5275affc8e543bc7fc23cd28fb853d85d9baf4a6
Carmen Alvarez <carmen@rmen.ca>
2025-01-18 17:31:29 +0100

    Update to the support library 22.1.0 to style the alert dialog.

--- app/src/main/java/ca/rmen/userlist/UserListActivity.java
+++ app/src/main/java/ca/rmen/userlist/UserListActivity.java
@@ -2,13 +2,13 @@ package ca.rmen.userlist;
 
 import java.util.List;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
