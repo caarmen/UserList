7408481a874b344e2dbcadc0b16491f4025bf17e
Carmen Alvarez <carmen@rmen.ca>
2025-01-18 19:32:10 +0100

    Use retrofit for network calls.

--- app/src/main/java/ca/rmen/userlist/UserRepository.java
+++ app/src/main/java/ca/rmen/userlist/UserRepository.java
@@ -1,35 +1,34 @@
 package ca.rmen.userlist;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.squareup.okhttp.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class UserRepository {
-    private final String sourceUrl = "http://rmen.ca/userlist/data.json";
-    private static final TypeToken<Response> RESPONSE = new TypeToken<Response>() {
-    };
-    private static final Gson GSON = new Gson();
 
     private static class Response {
         List<UserModel> results;
     }
 
+    public interface UserApi {
+        @GET("/userlist/data.json")
+        Call<Response> listUsers();
+    }
+
     public List<UserModel> fetchUsers() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://rmen.ca/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        UserApi userListApi = retrofit.create(UserApi.class);
+        Call<Response> call = userListApi.listUsers();
+
         try {
-
-            OkHttpClient client = new OkHttpClient();
-            HttpURLConnection connection = client.open(new URL(sourceUrl));
-            InputStream is = connection.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-
-            Response resp = GSON.fromJson(isr, RESPONSE.getType());
-            return resp.results;
+            return call.execute().body().results;
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
