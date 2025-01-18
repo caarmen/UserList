6feaf2aa9f48180313add9dab7dff97e95848bb3
Carmen Alvarez <carmen@rmen.ca>
2025-01-18 14:30:18 +0100

    Use glide (only works in android api level 8+) for image loading.

--- UserList/src/ca/rmen/userlist/UserListAdapter.java
+++ UserList/src/ca/rmen/userlist/UserListAdapter.java
@@ -2,12 +2,9 @@ package ca.rmen.userlist;
 
 import java.util.List;
 
-import ca.rmen.userlist.R;
+import com.bumptech.glide.Glide;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,35 +39,7 @@ public class UserListAdapter extends ArrayAdapter<UserModel> {
 		TextView nameView = (TextView) result.findViewById(R.id.name);
 		final ImageView avatarView = (ImageView) result.findViewById(R.id.avatar);
 		nameView.setText(user.name.first + " " + user.name.last);
-
-		// Set the image. Fetch the image in the background.
-		AsyncTask<UserModel, Void, Bitmap> previousTask = (AsyncTask<UserModel, Void, Bitmap>) result.getTag();
-		if (previousTask != null)
-			previousTask.cancel(true);
-		AsyncTask<UserModel, Void, Bitmap> avatarTask = new AsyncTask<UserModel, Void, Bitmap>() {
-
-			@Override
-			protected Bitmap doInBackground(UserModel... user) {
-				Log.d(TAG, "doInBackground, position = " + position + ", user = " + user[0].name.first);
-				return UserRepository.getAvatar(user[0]);
-			}
-
-			@Override
-			protected void onPostExecute(Bitmap bitmap) {
-				Log.d(TAG, "onPostExecute, isShown = " + result.isShown() + ", position = " + position + ", user = "
-						+ user.name.first);
-				// If the user is scrolling quickly, this view may not be for
-				// this avatar anymore.
-				// Libraries like Picasso and Glide handle this nicely for us,
-				// but they require more recent versions of Android.
-				if (!isCancelled()) {
-					avatarView.setImageBitmap(bitmap);
-				}
-			}
-
-		};
-		result.setTag(avatarTask);
-		avatarTask.execute(user);
+		Glide.load(user.picture.thumbnail).into(avatarView);
 		return result;
 	}
 
